f2bdbdcb56cffcb97a70d14c0a039b96c66f0351
Nellefb <ellenfebar@gmail.com>
2024-09-15 18:43:43 -0300

    Checkpoint Lista de Compras

---
+++ app/src/main/java/nellefb/com/github/listadecompras/ItemModel.kt
@@ -0,0 +1,11 @@
+package nellefb.com.github.listadecompras
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ItemModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String
+)
